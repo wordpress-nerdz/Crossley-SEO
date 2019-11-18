52f4cf654c01a2c5d75077bf48f732ec749b2bba
WordPress Nerdz <crossley@wordpressnerdz.com>
2019-11-18 21:23:49 +0200

    started the project

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="The Best SEO Ad Agency in South Africa">
+    <meta name="keywords" content="SEO">
+    <meta name="author" content="Cowies Hill Art Gallery">
+    <title>Crossley SEO</title>
+    <style><?php include "style.css" ?></style>
+</head>
+<body>
+    <header>
+    
+    </header>
+    <main>
+    
+    </main>
+    <footer>
+    
+    </footer>
+</body>
+</html>
